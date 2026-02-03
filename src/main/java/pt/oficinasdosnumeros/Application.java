b933bbef582e619ac63d74f58921ed68d700effd
Nuno Passos <nunompassos@gmail.com>
2026-02-03 09:01:02 +0000

    build: bootstrap Micronaut project with Java 25 and Gradle

---
+++ src/main/java/pt/oficinasdosnumeros/Application.java
@@ -0,0 +1,10 @@
+package pt.oficinasdosnumeros;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
